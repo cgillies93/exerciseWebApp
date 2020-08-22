19b99672a50e2d4d6eab55f1ade167cac58e0ba8
cgillies93 <59749520+cgillies93@users.noreply.github.com>
2020-08-21 17:04:00 -0700

    add articlelist component

---
+++ src/components/BlogArticleList/BlogArticleList.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './BlogArticleList.css';
+
+class BlogArticleList extends Component {
+  render() {
+    return(
+      <div className='blog-article-list-wrapper'>
+        <a href='#'>
+          <article className='blog-article'>
+            <div className='blog-article-image'>
+            </div>
+            <p>Fitness</p>
+            <h4>Blog Post Name</h4>
+          </article>
+        </a>
+        <a href='#'>
+          <article className='blog-article'>
+            <div className='blog-article-image'>
+            </div>
+            <p>Fitness</p>
+            <h4>Blog Post Name</h4>
+          </article>
+        </a>
+        <a href='#'>
+          <article className='blog-article'>
+            <div className='blog-article-image'>
+            </div>
+            <p>Fitness</p>
+            <h4>Blog Post Name</h4>
+          </article>
+        </a>
+        <a href='#'>
+          <article className='blog-article'>
+            <div className='blog-article-image'>
+            </div>
+            <p>Fitness</p>
+            <h4>Blog Post Name</h4>
+          </article>
+        </a>
+        <a href='#'>
+          <article className='blog-article'>
+            <div className='blog-article-image'>
+            </div>
+            <p>Fitness</p>
+            <h4>Blog Post Name</h4>
+          </article>
+        </a>
+        <a href='#'>
+          <article className='blog-article'>
+            <div className='blog-article-image'>
+            </div>
+            <p>Fitness</p>
+            <h4>Blog Post Name</h4>
+          </article>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default BlogArticleList;
